e4d1e4fed22be818de683508484026351a9d9d72
uehara <m19981108@gmail.com>
2025-05-25 10:58:52 +0900

    初回コミット

---
+++ src/main/java/raisetech/StudentManagement/StudentmanagementApplication.java
@@ -0,0 +1,18 @@
+package raisetech.StudentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class StudentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementApplication.class, args);
+	}
+
+
+
+	}
